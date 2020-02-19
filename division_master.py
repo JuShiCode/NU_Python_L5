2ee776b58eeb815d767d35537d84f0a92c5eae7e
JuShiBuka <strekosa@yandex.ru>
2020-02-19 16:35:26 +0500

    Task for Lesson 5

--- division_master.py
+++ division_master.py
@@ -9,63 +9,47 @@ def simple(x):
             if x % div == 0:
                 return False
         return True
-print(simple(2))
+# print(simple(2))
 
 # Вывод всех делителей числа
 def all_dividors(x):
     all_dividors_list = [i for i in range(1, x+1) if x%i == 0]
     return all_dividors_list
 # print (all_dividors(25))
+#
+# # #Выводит самый большой простой делитель числа
 
-# #Выводит самый большой простой делитель числа
 def max_simple_div(x):
-    simple_div_list = []
-    for i in range(x, 1, -1):
-        if x % i == 0 and simple(i) is True:
-            simple_div_list.append(i)
-            break
-        else:
-            continue
+    simple_div_list = [i for i in range(x, 1, -1) if x % i == 0 and simple(i) is True]
     return max(simple_div_list)
-# print(max_simple_div(25))
+# print(max_simple_div(77))
+
+# def max_simple_div(x):
+# simple_div_list = []
+#      for i in range(x, 1, -1):
+#          if x % i == 0 and simple(i) is True:
+#              simple_div_list.append(i)
+#              break
+#          else:
+#              continue
+#     return max(simple_div_list)
+# # print(max_simple_div(77))
 
 # Каноническое разложение числа на простые множители
 # (https://zaochnik.com/spravochnik/matematika/delimost/razlozhenie-chisel-na-prostye-mnozhiteli/)
-all_simple_dividors_list = []
-def all_simple_dividors(x):
+def canon_simple_dividors(x):
+    all_simple_dividors_list = []
     for n in range(2, int(x)):
         if x % n == 0:
             all_simple_dividors_list.append(n)
             x = x/n
-            return all_simple_dividors(x)
         else:
             n +=1
         continue
-    all_simple_dividors_list.append(int(x))
-    return all_simple_dividors_list
-# print(all_simple_dividors(50))
-def canon_simple_dividors(x):
-    list = [f'{div}^{all_simple_dividors_list.count(div)}' for div in set(all_simple_dividors(x))]
+    list = [f'{div}^{all_simple_dividors_list.count(div)}' for div in set(all_simple_dividors_list)]
     return list
-# print(canon_simple_dividors(55))
+# print(canon_simple_dividors(130))
 
-# all_simple_dividors_list = []
-# def all_simple_dividors(x):
-#     for n in range(2, int(x)):
-#         if x % n == 0:
-#             all_simple_dividors_list.append(n)
-#             x = x/n
-#             return all_simple_dividors(x)
-#         else:
-#             n +=1
-#         continue
-#     all_simple_dividors_list.append(int(x))
-#     return all_simple_dividors_list
-# # print(all_simple_dividors(50))
-# def canon_simple_dividors(x):
-#     for div in set(all_simple_dividors(x)):
-#         print(div, '^', all_simple_dividors_list.count(div))
-# print(canon_simple_dividors(55))
 
 # Самый большой делитель (не обязательно простой) числа.
 def max_dividor(x):
